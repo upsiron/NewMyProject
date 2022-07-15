ae829b32a246ca66c7fde78b5240348edd5d7093
upsilon_n <yuya20000414@gmail.com>
2022-07-15 10:00:25 +0900

    描画する障害物のFBX変更

--- Object.cpp
+++ Object.cpp
@@ -5,6 +5,6 @@ Object::Object()
 	Framework& framework = Framework::Instance();
 	ID3D11Device* device = framework.GetDevice();
 
-	obstacleMesh = std::make_shared<SkinnedMesh>(device, "Data/Stage/block2.fbx", true);
+	obstacleMesh = std::make_shared<SkinnedMesh>(device, "Data/Stage/StageBlock.fbx", true);
 	coinMesh = std::make_shared<SkinnedMesh>(device, "Data/Stage/block.fbx", true);
 }
